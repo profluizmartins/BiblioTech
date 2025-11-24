ff3e6bb63566668c79be421a5970ea730c2575cd
vrummmm <rafael_lemes@discente.ufg.br>
2025-11-24 17:58:33 -0300

    Entrega inicial1.1 Grupo 8

--- src/model/excecoes/DadosInsuficientesException.java
+++ src/model/excecoes/DadosInsuficientesException.java
@@ -1,6 +1,9 @@
 package model.excecoes;
 
 public class DadosInsuficientesException extends Exception {
+    /**
+     * Emite a excessão quando os dados são insuficientes.
+     */
     public DadosInsuficientesException(String mensagem) {
         super(mensagem);
     }
