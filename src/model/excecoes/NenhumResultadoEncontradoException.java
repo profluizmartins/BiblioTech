4eeeb65bff39a5a94fea876fa083eeca66e3f59c
vrummmm <rafael_lemes@discente.ufg.br>
2025-11-24 17:56:10 -0300

    Entrega inicial1.1 Grupo 8

--- src/model/excecoes/NenhumResultadoEncontradoException.java
+++ src/model/excecoes/NenhumResultadoEncontradoException.java
@@ -1,6 +1,9 @@
 package model.excecoes;
 
 public class NenhumResultadoEncontradoException extends Exception {
+    /**
+     * Emite a excessão quando nenhum resultado é encontrado
+     */
     public NenhumResultadoEncontradoException(String mensagem) {
         super(mensagem);
     }
