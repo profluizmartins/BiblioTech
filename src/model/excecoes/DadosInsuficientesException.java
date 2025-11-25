da464fec7e4021d84e9837da3fce86a1323630ef
VRUMMMM <rafael_lemes@discente.ufg.br>
2025-11-25 13:19:24 -0300

    Update DadosInsuficientesException.java

--- src/model/excecoes/DadosInsuficientesException.java
+++ src/model/excecoes/DadosInsuficientesException.java
@@ -2,9 +2,10 @@ package model.excecoes;
 
 public class DadosInsuficientesException extends Exception {
     /**
-     * Emite a excessão quando os dados são insuficientes.
+     * Emite a exceção quando os dados são insuficientes.
      */
     public DadosInsuficientesException(String mensagem) {
         super(mensagem);
     }
 }
+
