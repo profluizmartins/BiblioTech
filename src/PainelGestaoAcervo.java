1943d50ea8faf81e7df2b15efbcfb88d291ea6df
João Victor Felix Dos Santos <joao.vsantos@ojc.com.br>
2025-11-25 09:00:09 -0300

    Organizando arquivos na pasta src

--- src/PainelGestaoAcervo.java
+++ src/PainelGestaoAcervo.java
@@ -137,7 +137,7 @@ public class PainelGestaoAcervo extends JPanel {
         }
     }
 
-    // --- MÉTODO MAIN TESTE Y TESTE PROFESSOR (Só para você testar essa tela agora) ---
+    //  --- MÉTODO MAIN (Só para você testar essa tela agora) ---
     public static void main(String[] args) {
         JFrame janelaTeste = new JFrame("Teste do Grupo 2");
         janelaTeste.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -149,4 +149,4 @@ public class PainelGestaoAcervo extends JPanel {
         // Mostra a janela
         janelaTeste.setVisible(true);
     }
-}
+} 
