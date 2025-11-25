22d6843403c5328438502fdb79e9b8731f710c7f
VRUMMMM <rafael_lemes@discente.ufg.br>
2025-11-25 13:19:50 -0300

    Update NenhumResultadoEncontradoException.java

--- src/model/excecoes/NenhumResultadoEncontradoException.java
+++ src/model/excecoes/NenhumResultadoEncontradoException.java
@@ -2,9 +2,10 @@ package model.excecoes;
 
 public class NenhumResultadoEncontradoException extends Exception {
     /**
-     * Emite a excessão quando nenhum resultado é encontrado
+     * Emite a exceção quando nenhum resultado é encontrado
      */
     public NenhumResultadoEncontradoException(String mensagem) {
         super(mensagem);
     }
 }
+
