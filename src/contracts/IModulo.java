fd619f2c678fcc79b7297a8ca4ee5aa51199d1f8
cleiltonr <cleilton@discente.ufg.br>
2025-12-04 22:28:09 -0300

    Update IModulo.java

--- src/contracts/IModulo.java
+++ src/contracts/IModulo.java
@@ -1,5 +1,11 @@
 package contracts;
 
-public class IModulo {
+import javax.swing.JPanel;
+
+public interface IModulo {
+    // Retorna o painel principal do módulo
+    JPanel getPainel();
     
+    // Retorna o nome do módulo
+    String getTitulo();
 }
