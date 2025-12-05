b7ce4bac0f9244c646f6b7cb1357f0bcca64e561
cleiltonr <cleilton@discente.ufg.br>
2025-12-04 22:33:29 -0300

    Update TelaPrincipal.java

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -1,5 +1,7 @@
 package view;
 import javax.swing.*;
+import contracts.IModulo;
+
 import java.awt.*;
 /*import java.awt.event.ActionEvent;
 */
@@ -8,22 +10,22 @@ import java.awt.*;
  * 
  */
 public class TelaPrincipalView extends JFrame{
-	
+
+	//Componentes de layout e stts
+	private JPanel painelCentral;
+	private CardLayout cardLayout;
+	private JLabel lblStatus;
+
+	//Menu
 	JMenuBar barramenu;
+
 	//menu GESTÃO DE ACERVO
-	
 	JMenu menugestaoDeAcervo;
-	JMenuItem itemAdd1;
-	JMenuItem itemEditar1;
-	JMenuItem itemLer1;
-	JMenuItem itemRemover1;
+	JMenuItem adicionarObra, editarObra, listarObra, removerObra;
 	
 	//menu GESTÃO DE USUÁRIOS
 	JMenu menugestaoDeUsuarios;
-	JMenuItem itemAdd2;
-	JMenuItem itemEditar2;
-	JMenuItem itemLer2;
-	JMenuItem itemRemover2;
+	JMenuItem adicionarUsuario, editarUsuario, listarUsuario, removerUsuario;
 	
 	//menu GESTÃO DE EMPRÉSTIMOS
 	JMenu menugestaoDeEmprestimo;
@@ -49,45 +51,58 @@ public class TelaPrincipalView extends JFrame{
 	
 	//menu ADM SISTEMA
 	JMenu menuAdministracaoFuncionarios;
-	JMenuItem itemAdd3;
-	JMenuItem itemEditar3;
-	JMenuItem itemLer3;
-	JMenuItem itemRemover3;
-	
-	
+	JMenuItem adicionarFuncionario;
+	JMenuItem editarFuncionario;
+	JMenuItem listarFuncionario;
+	JMenuItem removerFuncionario;
 	
 	public TelaPrincipalView() {
-		
-		barramenu = new JMenuBar();
+        configurarJanela();
+        inicializarMenus();
+        inicializarAreaCentral(); //CardLayout
+        inicializarBarraStatus(); //Status
+    }
+
+	private void configurarJanela() {
+        setTitle("BiblioTech");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout()); //organizar Menu (Topo), Centro e Status (Rodapé)
+    }
+
+	private void inicializarMenus() {
+        barramenu = new JMenuBar();
+		setJMenuBar(barramenu); //Menu aqui
 		//menu gestão de acervo;
 		//cria a barra + adiciona na janela;
 		menugestaoDeAcervo = new JMenu("Gestão de Acervo");
 		barramenu.add(menugestaoDeAcervo);
 		//descreve cada item;
-		itemAdd1 = new JMenuItem("Adicionar Obra");
-		itemEditar1 = new JMenuItem("Editar Obra");
-		itemLer1 = new JMenuItem("Listar Obras do acervo");
-		itemRemover1 = new JMenuItem("Remover Obras do acervo");
+		adicionarObra = new JMenuItem("Adicionar Obra");
+		editarObra = new JMenuItem("Editar Obra");
+		listarObra = new JMenuItem("Listar Obras do acervo");
+		removerObra = new JMenuItem("Remover Obras do acervo");
 		//adiciona na barra de menu;
-		menugestaoDeAcervo.add(itemAdd1);
-		menugestaoDeAcervo.add(itemEditar1);
-		menugestaoDeAcervo.add(itemLer1);
-		menugestaoDeAcervo.add(itemRemover1);
+		menugestaoDeAcervo.add(adicionarObra);
+		menugestaoDeAcervo.add(editarObra);
+		menugestaoDeAcervo.add(listarObra);
+		menugestaoDeAcervo.add(removerObra);
 		
 		//menu gestao de usuarios
 		//cria a barra + adiciona na janela;
 		menugestaoDeUsuarios = new JMenu("Gestão de Usuários");
 		barramenu.add(menugestaoDeUsuarios);
 		//descreve cada item;
-		itemAdd2 = new JMenuItem("Novo usuario");
-		itemEditar2 = new JMenuItem("Editar usuario");
-		itemLer2 = new JMenuItem("Listar usuários");
-		itemRemover2 = new JMenuItem("Remover usuários");
+		adicionarUsuario = new JMenuItem("Novo usuario");
+		editarUsuario = new JMenuItem("Editar usuario");
+		listarUsuario = new JMenuItem("Listar usuários");
+		removerUsuario = new JMenuItem("Remover usuários");
 		//adiciona na barra de menu;
-		menugestaoDeUsuarios.add(itemAdd2);
-		menugestaoDeUsuarios.add(itemEditar2);
-		menugestaoDeUsuarios.add(itemLer2);
-		menugestaoDeUsuarios.add(itemRemover2);
+		menugestaoDeUsuarios.add(adicionarUsuario);
+		menugestaoDeUsuarios.add(editarUsuario);
+		menugestaoDeUsuarios.add(listarUsuario);
+		menugestaoDeUsuarios.add(removerUsuario);
 		
 		//menu gestao de emprestimos
 		//cria a barra + adiciona na janela;
@@ -143,59 +158,86 @@ public class TelaPrincipalView extends JFrame{
 		menuAdministracaoFuncionarios = new JMenu("Gestão de Funcionários");
 	    barramenu.add(menuAdministracaoFuncionarios);
 		//descreve cada item;
-		itemAdd3 = new JMenuItem("Novo Funcionário");
-		itemEditar3 = new JMenuItem("Editar Funcionário");
-		itemLer3 = new JMenuItem("Listar Funcionário");
-		itemRemover3 = new JMenuItem("Remover Funcionário");
+		adicionarFuncionario = new JMenuItem("Novo Funcionário");
+		editarFuncionario = new JMenuItem("Editar Funcionário");
+		listarFuncionario = new JMenuItem("Listar Funcionário");
+		removerFuncionario = new JMenuItem("Remover Funcionário");
 		//adiciona na barra de menu;
-		menuAdministracaoFuncionarios.add(itemAdd3);
-		menuAdministracaoFuncionarios.add(itemEditar3);
-		menuAdministracaoFuncionarios.add(itemLer3);
-		menuAdministracaoFuncionarios.add(itemRemover3);
+		menuAdministracaoFuncionarios.add(adicionarFuncionario);
+		menuAdministracaoFuncionarios.add(editarFuncionario);
+		menuAdministracaoFuncionarios.add(listarFuncionario);
+		menuAdministracaoFuncionarios.add(removerFuncionario);
 		
-		
-		//janelas (com todas as views prontas, poderemos completar essa parte do codigo)
-		
-		JPanel panelMenus = new JPanel();
-		panelMenus.setLayout(new GridLayout(1, 8));
-		panelMenus.add(barramenu);
-		panelMenus.setBackground(Color.gray);
-		//setLayout(new BorderLayout());
-		add(panelMenus, BorderLayout.NORTH);
-		/*
-		menugestaoDeAcervo.addActionListener(e -> trocarTela(new TelaHome()));
-		menugestaoDeUsuarios.addActionListener(e -> trocarTela(new TelaUsuarios()));
-		menugestaoDeEmprestimo.addActionListener(e -> trocarTela(new TelaProdutos()));
-		menugestaoDeDevolucoes.addActionListener(e -> trocarTela(new TelaHome()));
-		menugestaoDeReservas.addActionListener(e -> trocarTela(new TelaUsuarios()));
-		menugestaoDeMultas.addActionListener(e -> trocarTela(new TelaProdutos()));
-		menuPesquisaseRelatorios.addActionListener(e -> trocarTela(new TelaHome()));
-		menuAdministracaoFuncionarios.addActionListener(e -> trocarTela(new TelaUsuarios()));
-		*/ 
-		
-		setJMenuBar(barramenu);
-		setSize(800,600);
-		setTitle("BiblioTech");
-		setLocationRelativeTo(null);
-		setResizable(true);
-		this.getContentPane().add(panelMenus);
-		
-		JLabel lb = new JLabel("BEM VINDOS À BIBLIOTECH!",
-				SwingConstants.CENTER);
-		lb.setFont(new Font("Serif",Font.BOLD,45));
-		lb.setForeground(Color.black);
-		panelMenus.add(lb);
+		//Botão arquivo -> logout
+        JMenu menuArquivo = new JMenu("Arquivo");
+        JMenuItem itemSair = new JMenuItem("Sair");
+        itemSair.addActionListener(e -> logout()); // Chama o método que fecha a tela
+        menuArquivo.add(itemSair);
+        barramenu.add(menuArquivo);
 	}
-	public static void main(String[] args) {
-		TelaPrincipalView tv = new TelaPrincipalView();
-		//tv.pack();
-		tv.setVisible(true);
-		
-		
-		
-		
-	}
+		//
+		private void inicializarAreaCentral() {
+        // Implementação do CardLayout
+        cardLayout = new CardLayout();
+        painelCentral = new JPanel(cardLayout);
+        painelCentral.setBackground(Color.LIGHT_GRAY); //Cor de fundo só para teste
 
+        // Tela de Boas Vindas (Aparece quando abre)
+        JLabel labelBoasVindas = new JLabel("BEM VINDOS À BIBLIOTECH!", SwingConstants.CENTER);
+        labelBoasVindas.setFont(new Font("Serif", Font.BOLD, 45));
+        painelCentral.add(labelBoasVindas, "HOME"); // "HOME" é o nome dessa carta
 
+        add(painelCentral, BorderLayout.CENTER);
+    }
+	private void inicializarBarraStatus() {
+        //implementação da barra de status no rodapé
+        lblStatus = new JLabel(" Usuário não logado");
+        lblStatus.setBorder(BorderFactory.createEtchedBorder()); 
+        lblStatus.setPreferredSize(new Dimension(getWidth(), 30));
+        add(lblStatus, BorderLayout.SOUTH);
+    }
+	//Atualiza o texto do rodapé com o nome e cargo do usuário logado.
+    public void atualizarStatus(String nome, String cargo) {
+        lblStatus.setText(" Bem-vindo(a), " + nome + " | Cargo: " + cargo);
+    }
+	/**
+     * Adiciona o painel de um grupo ao CardLayout.
+     * @param modulo Interface do módulo do grupo
+     * @param nomeIdentificador Nome interno para o CardLayout (ex: "MODULO_ACERVO")
+     */
+    public void adicionarPainelModulo(IModulo modulo, String nomeIdentificador) {
+        painelCentral.add(modulo.getPainel(), nomeIdentificador);
+    }
+	/**
+     * Troca a tela central para o módulo desejado.
+     * Ex: mostrarPainel("MODULO_ACERVO")
+     */
+    public void mostrarPainel(String nomeIdentificador) {
+        cardLayout.show(painelCentral, nomeIdentificador);
+    }
 
+    private void logout() {
+        this.dispose();
+        new TelaLogin().setVisible(true);
+    }
+
+	// --- GETTERS DOS MENUS
+    // Para o Controller acessar
+
+    // Getters do Acervo
+    public JMenuItem getItemAdicionarObra() { return adicionarObra; }
+    public JMenuItem getItemListarObra() { return listarObra; }
+
+    // Getters de Usuários
+    public JMenuItem getItemAdicionarUsuario() { return adicionarUsuario; }
+    public JMenuItem getItemListarUsuario() { return listarUsuario; }
+
+    // Getters de Empréstimo
+    public JMenuItem getItemRealizarEmprestimo() { return realizarEmprestimo; }
+    
+    // Getters de Devolução
+    public JMenuItem getItemRealizarDevolucao() { return realizarDevolucao; }
+
+    // Getters de ADM
+	public JMenuItem getItemListarFuncionario() {return listarFuncionario;}
 }
