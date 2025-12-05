d07cd98889984883cfe6ad824fbace0585188740
cleiltonr <cleilton@discente.ufg.br>
2025-12-04 22:29:26 -0300

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,8 +2,11 @@ package controller;
 
 import exception.AutenticacaoFalhouException;
 import model.SessaoUsuario;
+import view.TelaPrincipalView;
 import model.Funcionario;
 import model.FuncionarioRepositorio;
+import java.awt.Color;
+import core.ModuloTeste;
 
 /**
  * Controlador responsável pelo processo de autenticação de usuários.
@@ -32,14 +35,28 @@ public class LoginController {
 
         Funcionario f = repositorio.buscarPorLogin(login);
 
-        if (f == null) {
-            throw new AutenticacaoFalhouException("Usuário ou senha inválidos.");
-        }
-
-        if (!f.getSenhaHash().equals(senha)) {
-            throw new AutenticacaoFalhouException("Usuário ou senha inválidos.");
+        // Validação unificada para não revelar se o erro é login ou senha
+        if (f == null || !f.getSenhaHash().equals(senha)) {
+            throw new AutenticacaoFalhouException("Erro de Login: Usuário ou senha inválidos. Tente novamente."); 
         }
 
         SessaoUsuario.getInstance().logar(f);
+        //Cria a View Principal
+    TelaPrincipalView telaPrincipal = new TelaPrincipalView();
+    telaPrincipal.atualizarStatus(f.getNome(), f.getCargo());
+    
+    //Cria o Controller do Menu
+        MenuController menuCtrl = new MenuController(telaPrincipal);
+    
+        //Carregando módulos falsos para testar a navegação
+        try {
+            telaPrincipal.adicionarPainelModulo(new ModuloTeste("Acervo", Color.CYAN), "MODULO_ACERVO");
+            telaPrincipal.adicionarPainelModulo(new ModuloTeste("Usuario", Color.PINK), "MODULO_USUARIO");
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar módulos de teste: " + e.getMessage());
+        }
+    
+        //Exibe a tela
+        telaPrincipal.setVisible(true);
     }
 }
